dba994c80639c39bd094accb47f1f2832d6551a5
Nilton Romero <niltondeveloper96@gmail.com>
2025-05-28 15:26:02 -0500

    add vendor

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,39 @@ Auth::routes(['register' => false]);
 
 //Solo para authenticated users
 Route::group(['middleware' => ['auth']], function () {
+
+    //Ejecutar migración
+    Route::get('/ejecutar-migraciones', function () {
+        Artisan::call('migrate');
+        return 'Migraciones ejecutadas con éxito.';
+    });
+
+    //Optimize
+    Route::get('/optimize', function () {
+        Artisan::call('optimize');
+        return 'Optimizado.';
+    });
+
+    //Cache todo
+    Route::get('/cache-todo', function () {
+        Artisan::call('optimize');
+        Artisan::call('cache:clear');
+        Artisan::call('config:clear');
+        Artisan::call('view:clear');
+        Artisan::call('route:clear');
+        // Artisan::call('livewire:discover');
+        return 'Cache todo.';
+    });
+
+    //storage link
+    Route::get('/storage-link', function () {
+        Artisan::call('storage:link');
+        return 'Storage link creado correctamente en cpanel.';
+    });
+
+
+
+
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     
     Route::resource('/employees', EmployeeController::class)->names('employees');
